49a493d18d9e62059573736ebb7dc301e100b1a2
rykerdz <rykerdz18@gmail.com>
2022-08-17 19:09:50 +0100

    Fixed large box

--- face_extractor.py
+++ face_extractor.py
@@ -3,10 +3,34 @@ import os
 import cv2
 import numpy as np
 
+def draw_border(img, pt1, pt2, color, thickness, r, d):
+    x1, y1 = pt1
+    x2, y2 = pt2
+
+    # Top left
+    cv2.line(img, (x1 + r, y1), (x1 + r + d, y1), color, thickness)
+    cv2.line(img, (x1, y1 + r), (x1, y1 + r + d), color, thickness)
+    cv2.ellipse(img, (x1 + r, y1 + r), (r, r), 180, 0, 90, color, thickness)
+
+    # Top right
+    cv2.line(img, (x2 - r, y1), (x2 - r - d, y1), color, thickness)
+    cv2.line(img, (x2, y1 + r), (x2, y1 + r + d), color, thickness)
+    cv2.ellipse(img, (x2 - r, y1 + r), (r, r), 270, 0, 90, color, thickness)
+
+    # Bottom left
+    cv2.line(img, (x1 + r, y2), (x1 + r + d, y2), color, thickness)
+    cv2.line(img, (x1, y2 - r), (x1, y2 - r - d), color, thickness)
+    cv2.ellipse(img, (x1 + r, y2 - r), (r, r), 90, 0, 90, color, thickness)
+
+    # Bottom right
+    cv2.line(img, (x2 - r, y2), (x2 - r - d, y2), color, thickness)
+    cv2.line(img, (x2, y2 - r), (x2, y2 - r - d), color, thickness)
+    cv2.ellipse(img, (x2 - r, y2 - r), (r, r), 0, 0, 90, color, thickness)
+
 # Define paths
 base_dir = os.path.dirname(__file__)
-prototxt_path = os.path.join(base_dir + 'model_data/deploy.prototxt')
-caffemodel_path = os.path.join(base_dir + 'model_data/weights.caffemodel')
+prototxt_path = os.path.join(base_dir + '/model_data/deploy.prototxt')
+caffemodel_path = os.path.join(base_dir + '/model_data/weights.caffemodel')
 
 # Read the model
 model = cv2.dnn.readNetFromCaffe(prototxt_path, caffemodel_path)
@@ -18,12 +42,14 @@ if not os.path.exists('faces'):
 
 # Loop through all images and strip out faces
 count = 0
-for file in os.listdir(base_dir + 'images'):
+for file in os.listdir(base_dir + '/images'):
 	file_name, file_extension = os.path.splitext(file)
 	if (file_extension in ['.png','.jpg']):
-		image = cv2.imread(base_dir + 'images/' + file)
-
-		(h, w) = image.shape[:2]
+		image = cv2.imread(base_dir + '/images/' + file)
+		try:
+			(h, w) = image.shape[:2]
+		except:
+			continue
 		blob = cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)), 1.0, (300, 300), (104.0, 177.0, 123.0))
 
 		model.setInput(blob)
@@ -41,7 +67,19 @@ for file in os.listdir(base_dir + 'images'):
 				count += 1
 				frame = image[startY:endY, startX:endX]
 				if frame.any():
-					cv2.resize(frame, (300, 300))
-					cv2.imwrite(base_dir + 'faces/' + str(i) + '_' + file, frame)
+					cv2.imwrite('faces/' + str(i) + '_' + file, frame) 
+					draw_border(image, (startX, startY),
+                     			(endX, endY), (225,249,126), 2, 5, 10)
+					cv2.putText(image, 'DETECTED FACE', (startX, startY-5),
+									cv2.FONT_HERSHEY_SIMPLEX, 0.5, (225,249,126), 1)
+
+					cv2.imwrite("results/"+ file_name + file_extension, image)
+					
+					#my_frame = cv2.resize(frame, (224, 224))
+					#grayscale = cv2.cvtColor(my_frame, cv2.COLOR_BGR2GRAY)
+					
 
 print("Extracted " + str(count) + " faces from all images")
+
+
+
